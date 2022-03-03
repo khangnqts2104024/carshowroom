f9707cfde4ba65a70cc09e6ec5705b161cbc6137
khangnqts2104024 <87858814+khangnqts2104024@users.noreply.github.com>
2022-03-03 16:51:40 +0700

    khang

--- app/Models/order.php
+++ app/Models/order.php
@@ -20,12 +20,15 @@ class order extends Model
 
         public $timestamps = false;
     public function orderDetail(){
-        return $this->hasMany(orderDetail::class);
-    }
+        return $this->hasMany(orderDetail::class,'order_id','order_id');
+    } //tra mảng
     public function customer(){
         return $this->belongsTo(Customer_Info::class,'customer_id','customer_id');
     }
     public function showrooms(){
         return $this->belongsTo(showroom::class,'showroom','id');
+    }
+    public function cars(){
+        return $this->hasMany(carInfo::class,'order_id','order_id');
     }
 }

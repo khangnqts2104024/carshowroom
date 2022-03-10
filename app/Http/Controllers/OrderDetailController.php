ff606aac336a322d57fffe54d67c36b2b0fd4008
khangnqts2104024 <87858814+khangnqts2104024@users.noreply.github.com>
2022-03-10 17:15:48 +0700

    kang

--- app/Http/Controllers/OrderDetailController.php
+++ app/Http/Controllers/OrderDetailController.php
@@ -101,10 +101,10 @@ class OrderDetailController extends Controller
     {
         $request->validate([
             'email' => array('required', 'regex:/^[^\s@-]+@[^\s@-]+\.[^\s@]+$/', 'unique:customer_accounts,email'),
-            'fullname' => array('required', 'regex:/^[A-Za-z\s\.]+$/'),
+            'fullname' => array('required', 'regex:/^([a-zA-Z0-9ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s]+)$/i'),
             'citizen_id' => array('required', 'regex:/^[0-9]*$/'),
             'phone_number' => array('required', 'regex:/^[0-9]{10,11}$/'),
-            'address' => array('required', 'regex:/^[a-zA-Z0-9,\-\s\.]*$/'),
+            'address' => array('required','regex:/^([a-zA-Z0-9ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s]+)$/i'),
             'orderprice' => 'required',
             'modelnote' => 'required',
         ]);

c05316dae571b9d05ae16ef2c7874e6e9e64df9e
khangnqts2104024 <87858814+khangnqts2104024@users.noreply.github.com>
2022-03-11 16:00:58 +0700

    khang check dttable

--- resources/views/admin/general/all_model.blade.php
+++ resources/views/admin/general/all_model.blade.php
@@ -136,7 +136,7 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
 <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+
 <script>
 	$('.mydatatable').DataTable();
 </script>
